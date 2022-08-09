5df81a0661f7ba037bc904e8786bac92f2f29127
七块钱的巧克力 <75053637+1536372533@users.noreply.github.com>
2022-08-09 16:27:23 +0800

    Add files via upload

---
+++ src/views/content/0.vue
@@ -0,0 +1,28 @@
+<template>
+<div>
+    <div id="scroll">
+        <Scroll></Scroll>
+    </div>
+</div>
+
+  
+</template>
+
+<script>
+
+import Scroll from '../../components/Scroll.vue'
+
+export default {
+    name: 'home',
+    components:{
+        Scroll,
+    }
+}
+</script>
+
+<style scoped>
+#scroll{
+    /* 元素的最小宽度 */
+    min-width: 1500px;
+}
+</style>
